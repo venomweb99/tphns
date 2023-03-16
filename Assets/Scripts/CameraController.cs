1067d7dd416e6c8bf72bb8ce3326ab3e150340d5
venomweb99 <venomweb99@gmail.com>
2023-03-16 16:33:52 +0100

    1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float cameraDistance = 10.0f;
+    public float cameraDistance = 5.0f;
     public float cameraCurrentSpeed = 0.0f;
     public float cameraMaxSpeed = 10.0f;
     public float cameraAcceleration = 2.0f;
@@ -19,30 +19,53 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //make the camera follow the player at distance slowly
+        //make the camera follow the player's back at distance both from the player and from the ground slowly at a dustance
         float dt = Time.deltaTime;
         Vector3 playerPos = player.transform.position;
         Vector3 cameraPos = transform.position;
-        Vector3 cameraDirection = playerPos - cameraPos;
-        cameraDirection.Normalize();
-        cameraCurrentSpeed += cameraAcceleration * dt;
-        cameraCurrentSpeed = Mathf.Min(cameraCurrentSpeed, cameraMaxSpeed);
-        transform.Translate(cameraDirection * cameraCurrentSpeed * dt);
 
-        //change acceleration based on distance and stop it when it gets close
-        float distance = Vector3.Distance(playerPos, cameraPos);
-        if (distance > cameraDistance)
+        //cast a ray from the camera to the ground to get the height and set the camera height to the ground height + camera distance unles the player is distance away from the camera
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
         {
-            cameraAcceleration = cameraTargetAcceleration;
+            //set the camera height to the ground height + camera distance unless the player is distance away from the camera
+            float cameraHeight = hit.point.y + cameraDistance;
+            if (playerPos.y > cameraHeight + cameraDistance)
+            {
+                cameraHeight = playerPos.y;
+            }
+            cameraPos.y = cameraHeight;
+
+            
         }
-        else if (distance < cameraDistance)
+
+        //move the camera towards the player
+        cameraCurrentSpeed = Mathf.Min(cameraCurrentSpeed + cameraAcceleration * dt, cameraMaxSpeed);
+        Vector3 cameraToPlayer = playerPos - cameraPos;
+        float cameraToPlayerLength = cameraToPlayer.magnitude;
+        if (cameraToPlayerLength > cameraDistance)
         {
-            cameraAcceleration = -cameraTargetAcceleration;
+            cameraToPlayer.Normalize();
+            cameraToPlayer *= cameraCurrentSpeed * dt;
+            cameraPos += cameraToPlayer;
         }
         else
         {
-            cameraAcceleration = 0.0f;
+            cameraCurrentSpeed = 0.0f;
         }
+
+        //set the camera position
+        transform.position = cameraPos;
+
+        
+        
+        
+
+        //rotate the camera vertically to look at the player
+        Vector3 cameraToLookAtPlayer = playerPos - cameraPos;
+        float angle = Mathf.Atan2(cameraToLookAtPlayer.x, cameraToLookAtPlayer.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
         
 
         //set the player rotation to the camera rotation horizontally only
