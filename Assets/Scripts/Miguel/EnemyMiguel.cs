43cbfe2579533f8b4382f056afef6ac63f430cd3
MiguelJarne <Alu.105275@usj.es>
2023-05-22 15:42:47 +0200

    Pull Puente/cuerdas, y algo más
    añadimos puente, y tocamos algunos fallos

--- Assets/Scripts/Miguel/EnemyMiguel.cs
+++ Assets/Scripts/Miguel/EnemyMiguel.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyMiguel : MonoBehaviour
 {
     #region public
     public GameObject Player;
+    //prueba
+    public Transform target;
     public float speed = 1.0f;
     public float DistaToPlayer = 5.0f;
     #endregion
@@ -49,14 +52,30 @@ public class EnemyMiguel : MonoBehaviour
     void GetCloser()
     {
         //we will get the direction of the player and move towards it
-        _target = Player.transform.position;
+        /*_target = Player.transform.position;
         _direction = _target - transform.position;
-        transform.position += _direction * speed * Time.deltaTime;
+        transform.position += _direction * speed;*/
+        Vector3 direction = target.position - transform.position;
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance > DistaToPlayer)
+        {
+            m_Rigidbody.velocity = direction.normalized * speed;
+        }
+        else
+        {
+            m_Rigidbody.velocity = Vector3.zero;
+        }
+
     }
     // if the enemy is in the range, we will draw the live of the enemy
     void LookAtPlayer()
     {
-        transform.LookAt(Player.transform.position);
+        int i = 0;
+        for (i = 0; i < 1; i++)
+        {
+            transform.LookAt(Player.transform.position);
+        }
     }
     //we will draw the distance between the player and the enemy
     void OnDrawGizmos()
