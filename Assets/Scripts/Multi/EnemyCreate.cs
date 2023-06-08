b4baa8a9c8088005ab6f97f7e33ef4371a7f3988
MiguelJarne <Alu.105275@usj.es>
2023-06-08 20:48:05 +0200

    sd
    sa

--- Assets/Scripts/Multi/EnemyCreate.cs
+++ Assets/Scripts/Multi/EnemyCreate.cs
@@ -8,10 +8,24 @@ using Unity.VisualScripting;
 
 public class EnemyCreate : NetworkBehaviour
 { 
+    public GameObject enemy;
+    private Transform prefabInstance;
     [SerializeField] private Transform prefab;
     private void Start()
     {
-        Transform prefabInstance = Instantiate(prefab);
+        prefabInstance = Instantiate(prefab);
         prefabInstance.GetComponent<NetworkObject>().Spawn(true);
+    }
+    private void Update()
+    {
+        //pillar la salud del enemigo
+        //si es menor o igual a 0
+        //destruir el prefab
+        //y updatevo id st
+
+        if(enemy.Health<=0)
+        {
+            prefabInstance.GetComponent<NetworkObject>().Despawn(true);
+        }
     }
 }
